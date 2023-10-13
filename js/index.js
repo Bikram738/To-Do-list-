7ddd04188bd52c137f496e3f406be6849f380a15
Maniruddin Khan <71460654+techeduhelper@users.noreply.github.com>
2023-10-13 20:18:11 +0530

    Update index.js
    Functionality will be same only change the code and readability

--- js/index.js
+++ js/index.js
@@ -1,21 +1,59 @@
-function Add() {
-  document.getElementById("invalid").innerHTML = "";
-  let addText = document.getElementById("text").value;
-  if (addText == "") {
-    document.getElementById("invalid").innerHTML = "Please enter some text";
+
+// Previous code
+// function Add() {
+//   document.getElementById("invalid").innerHTML = "";
+//   let addText = document.getElementById("text").value;
+
+// only you can compare the value not data type 
+
+//   if (addText == "") {
+//     document.getElementById("invalid").innerHTML = "Please enter some text";
+//     console.log("error");
+//   } else {
+//     document.getElementById(
+//       "list"
+//     ).innerHTML += `<li>${addText} <i class="fa-solid fa-trash" id="del" onclick="del(event)"></i></li>`; 
+//   }
+//    addText = document.getElementById("text").value="";
+//    addText = document.getElementById("text").focus();
+// }
+
+// function del(event) {
+//   let list= event.target.parentElement
+//   list.remove()
+// }
+
+
+// udated code 
+const invalidMessage = document.getElementById("invalid");
+const textInput = document.getElementById("text");
+const listContainer = document.getElementById("list");
+
+function clearInputAndFocus() {
+  textInput.value = "";
+  textInput.focus();
+}
+
+function addListItem(text) {
+  const listItem = document.createElement("li");
+  listItem.innerHTML = `${text} <i class="fa-solid fa-trash" id="del"></i>`;
+  listItem.querySelector("#del").addEventListener("click", del);
+  listContainer.appendChild(listItem);
+}
+
+function validateAndAdd() {
+  const addText = textInput.value;
+  if (addText === "") {
+    invalidMessage.innerHTML = "Please enter some text";
     console.log("error");
   } else {
-    document.getElementById(
-      "list"
-    ).innerHTML += `<li>${addText} <i class="fa-solid fa-trash" id="del" onclick="del(event)"></i></li>`; 
+    invalidMessage.innerHTML = "";
+    addListItem(addText);
+    clearInputAndFocus();
   }
-   addText = document.getElementById("text").value="";
-   addText = document.getElementById("text").focus();
 }
 
 function del(event) {
-  let list= event.target.parentElement
-  list.remove()
+  event.target.parentElement.remove();
 }
-
 
